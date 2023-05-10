883e3edece7a9a498ab1ae79f1041852eba4be13
NicholasConti <conti94@live.it>
2023-05-10 15:25:52 +0200

    feat:migration done

---
+++ database/migrations/2023_05_10_125019_create_comics_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('src');
+            $table->float('price', 6, 2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 100);
+            $table->string('artists'); //not sure
+            $table->string('writers'); //not sure
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
